2db430ea74bfef1f80436d1ea7f8c5cf713ef28c
Spaghetti Jones <connor.breault@comcast.net>
2019-12-03 19:21:23 -0800

    contact page forms added

--- assets/Js/logic.js
+++ assets/Js/logic.js
@@ -15,7 +15,7 @@ function js_Load() {
 $("#submitOrderBtn").on("click", function () {
     let Product = $("#productSelect").val().trim()
     let Color = $("#colorSelect").val().trim()
-    // let Size = $("#psizeSelect").val().trim()
+    let Size = $("#sizeSelect").val().trim()
     let Quantity = $("#quantitySelect").val().trim()
     let File = $("#fileInput").val().trim()
     let First = $("#first_name").val().trim()
@@ -26,5 +26,9 @@ $("#submitOrderBtn").on("click", function () {
     let State = $("#state").val().trim()
     let Address = $("#address").val().trim()
     let Zipcode = $("#zip_code").val().trim()
-    alert(`Product: ${Product} -- Color: ${Color} -- Quantity: ${Quantity} -- File: ${File} -- First: ${First} -- Last: ${Last} -- Phone: ${Phone} -- Email: ${Email} -- City: ${City} -- State: ${State} -- Address: ${Address} -- Zipcode: ${Zipcode}`)
+    if (Product === "" || Color === "" || Size === "" || Quantity === "" || File === "" || First === "" || Last === "" || Phone === "" || Email === "" || City === "" || State === "" || Address === "" || Zipcode === "") {
+        alert("Please fill out all forms")
+    } else {
+        alert(`Product: ${Product} -- Color: ${Color} -- Size: ${Size} -- Quantity: ${Quantity} -- File: ${File} -- First: ${First} -- Last: ${Last} -- Phone: ${Phone} -- Email: ${Email} -- City: ${City} -- State: ${State} -- Address: ${Address} -- Zipcode: ${Zipcode}`)
+    }
 })
