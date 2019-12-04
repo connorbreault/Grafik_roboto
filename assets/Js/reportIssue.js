d49768e68cbc0d3f5184cb88201b4409f9afc859
Spaghetti Jones <connor.breault@comcast.net>
2019-12-03 22:27:21 -0800

    contact us page fully functioning

---
+++ assets/Js/reportIssue.js
@@ -0,0 +1,37 @@
+// === MATERIALIZE FUNCTIONS === //
+$(document).ready(function () {
+    M.AutoInit();
+});
+function js_Load() {
+    document.body.style.visibility = 'visible'
+}
+
+$("#submitMessageBtn").on("click", function () {
+    let Name = $("#icon_prefix").val().trim()
+    let Phone = $("#icon_telephone").val().trim()
+    let Email = $("#icon_email").val().trim()
+    let Message = $("#messageBox").val().trim()
+    if (Name === "" || Phone === "" || Email === "" || Message === "") {
+        alert("Please fill out all fields")
+    } else {
+        $("#contactBox").addClass("hidden")
+        $("#loadingDiv").removeClass("hidden")
+        let templateParams = {
+            type: "MESSAGE",
+            name: Name,
+            number: Phone,
+            email: Email,
+            message: Message,
+        }
+        emailjs.send('default_service', 'grafikmessage', templateParams)
+            .then(function (response) {
+                console.log('Sucessful message send!')
+                $("#loadingDiv").addClass("hidden")
+                $("#thanksDiv").removeClass("hidden")
+            }, function (error) {
+                console.log('FAILED...', error)
+                $("#loadingDiv").addClass("hidden")
+                $("#errorDiv").removeClass("hidden")
+            });
+    }
+})
