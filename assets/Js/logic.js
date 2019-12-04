3790e4fc50716ef6a3dcf9065daed2aceeb3ea1e
Spaghetti Jones <connor.breault@comcast.net>
2019-12-03 16:25:13 -0800

    all values working for submit except photo input,

--- assets/Js/logic.js
+++ assets/Js/logic.js
@@ -10,3 +10,19 @@ $(document).ready(function () {
 
 
 // === SUBMIT ORDERS === //
+$("#submitOrderBtn").on("click", function () {
+    let Product = $("#productSelect").val().trim()
+    let Color = $("#colorSelect").val().trim()
+    // let Size = $("#psizeSelect").val().trim()
+    let Quantity = $("#quantitySelect").val().trim()
+    let File = $("#fileInput").val().trim()
+    let First = $("#first_name").val().trim()
+    let Last = $("#last_name").val().trim()
+    let Phone = $("#icon_telephone").val().trim()
+    let Email = $("#email").val().trim()
+    let City = $("#city").val().trim()
+    let State = $("#state").val().trim()
+    let Address = $("#address").val().trim()
+    let Zipcode = $("#zip_code").val().trim()
+    alert(`Product: ${Product} -- Color: ${Color} -- Quantity: ${Quantity} -- File: ${File} -- First: ${First} -- Last: ${Last} -- Phone: ${Phone} -- Email: ${Email} -- City: ${City} -- State: ${State} -- Address: ${Address} -- Zipcode: ${Zipcode}`)
+})
