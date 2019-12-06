46f64ca65d70971bcee84ec39af2c182f0cbba6e
Spaghetti Jones <connor.breault@comcast.net>
2019-12-05 19:39:27 -0800

    product imgs replaced

--- assets/Js/logic.js
+++ assets/Js/logic.js
@@ -13,20 +13,20 @@ function js_Load() {
 
 // === SUBMIT ORDERS === //
 $("#submitOrderBtn").on("click", function () {
-    let Product = $("#productSelect").val().trim()
-    let Color = $("#colorSelect").val().trim()
-    let Size = $("#sizeSelect").val().trim()
-    let Quantity = $("#quantitySelect").val().trim()
-    let File = $("#linkImage").val().trim()
-    let First = $("#first_name").val().trim()
-    let Last = $("#last_name").val().trim()
-    let Phone = $("#icon_telephone").val().trim()
-    let Email = $("#email").val().trim()
-    let City = $("#city").val().trim()
-    let State = $("#state").val().trim()
-    let Address = $("#address").val().trim()
-    let Zipcode = $("#zip_code").val().trim()
-    if (Product === "" || Color === "" || Size === "" || Quantity === "" || File === "" || First === "" || Last === "" || Phone === "" || Email === "" || City === "" || State === "" || Address === "" || Zipcode === "") {
+    let Product = $("#productSelect").val()
+    let Color = $("#colorSelect").val()
+    let Size = $("#sizeSelect").val()
+    let Quantity = $("#quantitySelect").val()
+    let File = $("#linkImage").val()
+    let First = $("#first_name").val()
+    let Last = $("#last_name").val()
+    let Phone = $("#icon_telephone").val()
+    let Email = $("#email").val()
+    let City = $("#city").val()
+    let State = $("#state").val()
+    let Address = $("#address").val()
+    let Zipcode = $("#zip_code").val()
+    if (Product === null || Color === null || Size === null || Quantity === null || File === "" || First === "" || Last === "" || Phone === "" || Email === "" || City === "" || State === "" || Address === "" || Zipcode === "") {
         alert("Please fill out all forms")
     } else {
         alert(`Product: ${Product} -- Color: ${Color} -- Size: ${Size} -- Quantity: ${Quantity} -- *File: ${File}* -- First: ${First} -- Last: ${Last} -- Phone: ${Phone} -- Email: ${Email} -- City: ${City} -- State: ${State} -- Address: ${Address} -- Zipcode: ${Zipcode}`)
