a3503aa33ed3334876d41aa843aa8dbfe436aeda
Spaghetti Jones <connor.breault@comcast.net>
2019-12-09 00:54:25 -0800

    items adding to cart

--- public/js/logic.js
+++ public/js/logic.js
@@ -23,37 +23,95 @@ $("#submitStandardBtn").on("click", function () {
         Product,
         Size,
         Quantity,
-        File
+        File,
     }
     cartItems.push(newItem)
     updateCart()
-    alert(cartItems)
 })
 $("#submitPremiumBtn").on("click", function () {
-    let premiumProduct = $("#premiumProductSelect").val()
-    let premiumSize = $("#premiumSizeSelect").val()
-    let premiumQuantity = $("#premiumQuantitySelect").val()
-    let premiumFile = $("#premiumLinkImage").val()
-    alert(`${premiumProduct} ${premiumQuantity} ${premiumSize} ${premiumFile}`)
+    let Product = $("#premiumProductSelect").val()
+    let Size = $("#premiumSizeSelect").val()
+    let Quantity = $("#premiumQuantitySelect").val()
+    let File = $("#premiumLinkImage").val()
+    let newItem = {
+        Product,
+        Size,
+        Quantity,
+        File,
+    }
+    cartItems.push(newItem)
+    updateCart()
 })
 $("#submitHouseBtn").on("click", function () {
-    let houseProduct = $("#houseProductSelect").val()
-    let houseSize = $("#houseSizeSelect").val()
-    let houseQuantity = $("#houseQuantitySelect").val()
-    let houseFile = $("#houseLinkImage").val()
-    alert(`${houseProduct} ${houseQuantity} ${houseSize} ${houseFile}`)
+    let Product = $("#houseProductSelect").val()
+    let Size = $("#houseSizeSelect").attr("data-value")
+    let Price = $("#houseSizeSelect").val()
+    let Quantity = $("#houseQuantitySelect").val()
+    let itemTotal = (Price * Quantity)
+    let File = $("#houseLinkImage").val()
+    alert(`${itemTotal} = SIZE=${Size} => PRICE=${Price} QUANTITY=${Quantity}`)
+    // let newItem = {
+    //     Product,
+    //     Size,
+    //     Price,
+    //     Quantity,
+    //     File,
+    //     itemTotal
+    // }
+    // cartItems.push(newItem)
+    // updateCart()
 })
 
 function updateCart() {
     $(".cartDiv").empty()
     for (let i = 0; i < cartItems.length; i++) {
-        `
-        <div>
-            <h1>${cartItems[i]}</h1>
+        let newItem = `
+        <div class="cartList">
+        <h2>Item ${[i + 1]}</h2>
+        <div class="row"> 
+        <div class="col s12 l4"> 
+        <h3><div class="cartItemInfoHead">Product: </div>${cartItems[i].Product}</h3> 
+        </div> 
+        <div class="col s6 l4"> 
+        <h3><div class="cartItemInfoHead">Quantity: </div>${cartItems[i].Quantity}</h3> 
+        </div> 
+        <div class="col s6 l4"> 
+        <h3><div class="cartItemInfoHead">Size: </div>${cartItems[i].Size}</h3> 
+        </div> 
+        </div> 
+        <div class="row"> 
+        <div class="col s12 l4"> 
+        <h3><div class="cartItemInfoHead">Price: </div>${cartItems[i].Price}</h3> 
+        </div> 
+        <div class="col s6 l4"> 
+        <h3><div class="cartItemInfoHead">Total: </div>${cartItems[i].itemTotal}</h3> 
+        </div> 
         </div>
-        `
+        <div class="row">
+        <div class="col s12 l8">
+        <p><div class="cartItemInfoHead">Img: </div>${cartItems[i].File}</p> 
+        </div> 
+        <div class="col s12 l2">
+        <a class="btn-floating btn-large waves-effect waves-light red removeCartItem" value="${[i]}"><i class="material-icons">delete</i></a>
+        </div> 
+        </div>
+        </div>`
+        $(".cartDiv").append(newItem)
     }
 }
+
+// $(".removeCartItem").addEventListener("click", function () {
+//     alert("hi")
+//     this.parentElement.parentElement.parentElement.remove()
+// })
+$(document.body).on("click", ".removeCartItem", function () {
+    let value = $(this).val()
+    // this.parentElement.parentElement.parentElement.remove()
+    if (value > -1) {
+        cartItems.splice(value, 1);
+    }
+    updateCart()
+})
 
 $("#submitOrderBtn").on("click", function () {
     let First = $("#first_name").val()
