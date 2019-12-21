66791667cfbdea2aa82be97121085743884ab85b
Spaghetti Jones <connor.breault@comcast.net>
2019-12-20 23:59:35 -0800

    test edit

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,17 +5,14 @@ module.exports = function (app) {
     app.get("/", function (req, res) {
         res.render("index");
     });
-
     // Load contact page
     app.get("/contact", function (req, res) {
         res.render("contact");
     });
-
     // Load report issue page
     app.get("/reportIssue", function (req, res) {
         res.render("reportIssue");
     });
-
     // Render 404 page for any unmatched routes
     // app.get("*", function (req, res) {
     //     res.render("404");
