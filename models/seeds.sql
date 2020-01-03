43e00407d78a4be4a07aed244598333a91c33ab5
Spaghetti Jones <connor.breault@comcast.net>
2020-01-02 14:48:29 -1000

    call cleanups

--- models/seeds.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO recentEnding
-    (ending, name)
-VALUES
-    ("Death", "Connor");
-INSERT INTO mainDialogue
-    (question, option1, option2)
-VALUES
-    ("What do you want to do today?", "Visit a Friend", "Run an Errand");
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,30 +1,4 @@
 // var db = require("../models");
 
 // module.exports = function (app) {
-//     // Get all questions
-//     app.get("/api/mainDialogue", function (req, res) {
-//         db.mainDialogue.findAll().then(function (mainDialogue) {
-//             res.json(mainDialogue)
-//         });
-//     });
-
-//     app.get("/api/adventureOne", function (req, res) {
-//         db.adventureOne.findAll().then(function (adventureOne) {
-//             res.json(adventureOne)
-//         });
-//     });
-
-//     // Create a new recent ending
-//     app.post("/api/recentEnding", function (req, res) {
-//         db.recentEnding.create(req.body).then(function (recentEnding) {
-//             res.json(recentEnding);
-//         });
-//     });
-
-//     // Delete an example by id
-//     // app.delete("/api/questions/:id", function (req, res) {
-//     //     db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
-//     //         res.json(dbExample);
-//     //     });
-//     // });
 // };
